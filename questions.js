4bdf2ad5a69de764d1a5dd0c87416a72d9139d6d
Jarred Washington <jarredw26@gmail.com>
2022-04-21 21:28:46 -0500

    Questions added.

---
+++ questions.js
@@ -0,0 +1,46 @@
+export default [
+    {
+        type: 'list',
+        name: 'q1',
+        message: 'Who created the Instagram app?',
+        choices: [
+            'Kevin Systrom',
+            'Mark Zuckerberg',
+            'Evan Spiegel',
+            'Marc Benioff'
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q2',
+        message: 'What is the first company Elon Musk created?',
+        choices: [
+            'Space-X',
+            'Tesla',
+            'Zip2',
+            'Paypal'
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q3',
+        message: 'Which mobile payment provider is the best?',
+        choices: [
+            'Zelle',
+            'Cash App',
+            'Venmo',
+            'Apple Pay'
+        ]
+    },
+    {
+        type: 'list',
+        name: 'q4',
+        message: 'Which is the most popular social media app?',
+        choices: [
+            'Snapchat',
+            'Instagram',
+            'Facebook',
+            'TikTok'
+        ]
+    },
+]
